d180b01b374c59b39713d4c236fa05ad84e48324
zhuhao-nju <40872587+zhuhao-nju@users.noreply.github.com>
2023-06-04 03:16:18 +0800

    Update download_model_njudrive.sh

--- checkpoints/download_model_njudrive.sh
+++ checkpoints/download_model_njudrive.sh
@@ -1,2 +1,2 @@
-wget --no-check-certificate 'https://box.nju.edu.cn/f/74325bd99c774879b0ac/?dl=1' -O ./checkpoints/checkpoints.zip
+wget --no-check-certificate 'https://box.nju.edu.cn/f/a0e7feafd091497eba5a/?dl=1' -O ./checkpoints/checkpoints.zip
 unzip -o ./checkpoints/checkpoints.zip -d ./checkpoints/
